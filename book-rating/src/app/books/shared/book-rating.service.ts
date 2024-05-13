819e717e7a19082f06a7e063a4c4f923b754e68b
Ferdinand Malcher <ferdinand@malcher.media>
2024-05-13 16:58:03 +0200

    Service und Immutability

---
+++ book-rating/src/app/books/shared/book-rating.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Book } from './book';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookRatingService {
+
+  constructor() { }
+
+  rateUp(book: Book): Book {
+    // nicht gut, weil es Immutability verletzt
+    // book.rating++;
+
+    // Early Exit
+    if (book.rating >= 5) {
+      return book;
+    }
+
+    return {
+      ...book,
+      rating: book.rating + 1
+    }
+  }
+
+  rateDown(book: Book): Book {
+    return {
+      ...book,
+      // rating: book.rating <= 1 ? 1 : book.rating - 1,
+      rating: Math.max(book.rating - 1, 1)
+    }
+  }
+
+
+
+}
+
+
