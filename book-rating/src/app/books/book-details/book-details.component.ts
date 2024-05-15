4feecebe9ade5fa82d98439872b69ce1ddd332fd
Ferdinand Malcher <ferdinand@malcher.media>
2024-05-15 13:07:28 +0200

    Detailseite: Bücher abrufen

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,30 +1,34 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
+import { BookStoreService } from '../shared/book-store.service';
+import { Book } from '../shared/book';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-book-details',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, JsonPipe],
   templateUrl: './book-details.component.html',
   styleUrl: './book-details.component.scss'
 })
 export class BookDetailsComponent {
 
+  book?: Book;
+
   // private route = inject(ActivatedRoute);
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private bs: BookStoreService) {
     // PULL: abfragen, wenn wir Parameter benötigen
     // const isbn = this.route.snapshot.paramMap.get('isbn');
     // console.log(isbn);
 
     // PUSH: wir werden informiert, wenn sich Parameter ändern
+    // TODO: verschachtelte Subscriptions vermeiden
     this.route.paramMap.subscribe(params => {
       const isbn = params.get('isbn')!; // Non-Null Assertion. Vorsicht, gefährlich!
-      console.log(isbn);
-
-      // AUFGABE:
-      // - HTTP Buch abrufen
-      // - Buch anzeigen (ganz einfach!)
+      this.bs.getSingle(isbn).subscribe(book => {
+        this.book = book;
+      });
     });
   }
 }
