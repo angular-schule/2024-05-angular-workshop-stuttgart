29e10456d18245e98900ebd908eab81325a9bb5b
Ferdinand Malcher <ferdinand@malcher.media>
2024-05-15 14:06:00 +0200

    Vorbereitung Formulare und Suche

---
+++ book-rating/src/app/books/book-search/book-search.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-search',
+  standalone: true,
+  imports: [],
+  templateUrl: './book-search.component.html',
+  styleUrl: './book-search.component.scss'
+})
+export class BookSearchComponent {
+
+}
--- book-rating/src/app/books/books.routes.ts
+++ book-rating/src/app/books/books.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from "@angular/router";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { BookDetailsComponent } from "./book-details/book-details.component";
+import { BookCreateComponent } from "./book-create/book-create.component";
+import { BookSearchComponent } from "./book-search/book-search.component";
 
 
 
 export const booksRoutes: Routes = [
   { path: 'books', component: DashboardComponent },
-  { path: 'books/:isbn', component: BookDetailsComponent }
+  { path: 'books/create', component: BookCreateComponent },
+  { path: 'books/search', component: BookSearchComponent },
+  { path: 'books/:isbn', component: BookDetailsComponent },
 ];
